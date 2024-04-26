eb2273570c83ebf3572581268c1a7a7c941dafbd
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-26 14:23:25 +0530

    Add Button component for login functionality

---
+++ src/ui/button.jsx
@@ -0,0 +1,11 @@
+
+
+const Button = () => {
+  return (
+    <button className="bg-gradient-to-b from-[#06286e] to-[#164ec0] text-white flex px-10 py-3 rounded-full capitalize">
+      Login
+    </button>
+  );
+};
+
+export default Button;
