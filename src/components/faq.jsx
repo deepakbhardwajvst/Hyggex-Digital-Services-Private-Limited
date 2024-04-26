c93245f1553d3eb72f7990e84a1c6e71d6c83fda
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-26 14:22:11 +0530

    Add FAQ component with multiple MultySelectInput components for user questions.

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
 
-const faq = () => {
+import MultySelectInput from "../ui/multyselectinput.jsx";
+
+const Faq = () => {
   return (
-    <div>faq</div>
-  )
-}
+    <div className="mt-[154px] mb-20">
+      <div>
+        <h1 className="uppercase font-bold text-[48px] bg-gradient-to-b from-[#06286e] to-[#164ec0] inline-block text-transparent bg-clip-text">
+          faq
+        </h1>
+      </div>
+      <div className="flex flex-col gap-8">
+        <MultySelectInput placeholder="Can education flashcards be used for all age groups?" />
+        <MultySelectInput placeholder="How do education flashcards work?" />
+        <MultySelectInput placeholder="Can education flashcards be used for test preparation?" />
+      </div>
+    </div>
+  );
+};
 
-export default faq
+export default Faq;
