bc17d11128ed08c8366c380e69fe12835e6591c4
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-26 14:19:30 +0530

    Add Navbar component with logo, navigation items, and login button. Utilize flexbox for layout, include navigation data from an array, and integrate a Button component for login functionality.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
 
-const navbar = () => {
+import logo from "../assets/navlogo.png";
+import Button from "../ui/button.jsx";
+
+const navData = [
+  {
+    id: "1",
+    name: "home",
+  },
+  {
+    id: "2",
+    name: "flashcard",
+  },
+  {
+    id: "3",
+    name: "contact",
+  },
+  {
+    id: "4",
+    name: "FAQ",
+  },
+];
+
+const Navbar = () => {
   return (
-    <div>navbar</div>
-  )
-}
+    <div className="flex justify-between items-center py-3 ">
+      <div className="w-[150px]">
+        <img src={logo} alt="logo image" className="cursor-pointer" />
+      </div>
+      <dlv className="flex gap-[40px] justify-center items-center">
+        <ul className="flex gap-[40px] text-[#3a3740] capitalize font-[400px] cursor-pointer">
+          {navData?.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+        <div>
+          <Button name="login" />
+        </div>
+      </dlv>
+    </div>
+  );
+};
 
-export default navbar
+export default Navbar;
